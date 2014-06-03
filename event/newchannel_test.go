781e2d4cfc21a22bd7cf939cd42e07eda40598c1
Jovany Leandro G.C <bit4bit@riseup.net>
2014-06-02 20:09:43 -0500

    test Newchannel

---
+++ event/newchannel_test.go
@@ -0,0 +1,33 @@
+package event
+
+import (
+	"github.com/bit4bit/gami"
+	"testing"
+)
+
+func TestNewchannel(t *testing.T) {
+	fixture := map[string]string{
+		"Channel": "Channel",
+		"Channelstate": "ChannelState",
+		"Channelstatedesc": "ChannelStateDesc",
+		"Calleridnum": "CallerIDNum",
+		"Calleridname": "CallerIDName",
+		"Accountcode": "AccountCode",
+		"Uniqueid": "UniqueID",
+		"Context": "Context",
+		"Exten": "Extension",
+	}
+
+	ev := gami.AMIEvent{
+		Id: "Newchannel",
+		Privilege: []string{"all"},
+		Params: fixture,
+	}
+
+	evtype := New(&ev)
+	if _, ok := evtype.(Newchannel); !ok {
+		t.Fatal("Newchannel type assertion")
+	}
+
+	testEvent(t, fixture, evtype)
+}
