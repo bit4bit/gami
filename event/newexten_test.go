5a48dc1943c7732176cddc5c553a8a8796702a8b
Jovany Leandro G.C <bit4bit@riseup.net>
2014-06-02 19:53:39 -0500

    test Newexten

---
+++ event/newexten_test.go
@@ -0,0 +1,29 @@
+package event
+
+import (
+	"github.com/bit4bit/gami"
+	"testing"
+)
+
+func TestNewexten(t *testing.T) {
+	fixture := map[string]string{
+		"Channel": "Channel",
+		"Extension": "Extension",
+		"Context": "Context",
+		"Priority": "Priority",
+		"Application": "Application",
+	}
+
+	ev := gami.AMIEvent{
+		Id: "Newexten",
+		Privilege: []string{"all"},
+		Params: fixture,
+	}
+	
+	evtype := New(&ev)
+	if _, ok := evtype.(Newexten); !ok {
+		t.Fatal("Newexten type assertion")
+	}
+
+	testEvent(t, fixture, evtype)
+}
