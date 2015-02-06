04368a876065b16f595ee64b14597a22814f32a0
Jovany Leandro G.C <bit4bit@riseup.net>
2015-02-06 17:38:58 -0500

    doc

--- gami.go
+++ gami.go
@@ -1,4 +1,61 @@
 // Package gami provites primitives for interacting with Asterisk AMI
+/*
+
+Basic Usage
+
+
+	ami, err := gami.Dial("127.0.0.1:5038")
+	if err != nil {
+		fmt.Print(err)
+		os.Exit(1)
+	}
+	defer ami.Close()
+
+	//install manager
+	go func() {
+		for {
+			select {
+			//handle network errors
+			case err := <-ami.NetError:
+				log.Println("Network Error:", err)
+				//try new connection every second
+				for {
+					<-time.After(time.Second)
+					if err := ami.Reconnect(); err == nil {
+						//call start actions
+						if _, err := ami.Action("Events", gami.Params{"EventMask": "on"}); err != nil {
+							break
+						}
+					}
+				}
+
+			case err := <-ami.Error:
+				log.Println("error:", err)
+			//wait events and process
+			case ev := <-ami.Events:
+				log.Println("Event Detect: %v", *ev)
+				//if want type of events
+				log.Println("EventType:", event.New(ev))
+			}
+		}
+	}()
+
+	if err := ami.Login("admin", "root"); err != nil {
+		fmt.Print(err)
+	}
+
+
+	if rs, err = ami.Action("Ping", nil); err != nil {
+		fmt.Print(rs)
+	}
+	if rs, err = ami.Action("Events", ami.Params{"EventMask":"on"}); err != nil {
+		fmt.Print(err)
+	}
+
+
+
+
+*/
 package gami
 
 import (
@@ -52,7 +109,7 @@ type AMIEvent struct {
 
 	Privilege []string
 
-	//Rest of arguments received
+	// Params  of arguments received
 	Params map[string]string
 }
 
